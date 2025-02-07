fe1321c410f2d5f0c4f22f845c897d3ac91637b0
aleka-tay <almarwa.amirtay@gmail.com>
2025-02-07 15:14:07 +0100

    create colors for categories

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,11 @@
 module CategoriesHelper
+  def colors_by_type(category)
+    colors={ pink: "--bs-danger-bg-subtle", green: "--bs-success-bg-subtle" }
+
+    if category&color.present?
+      colors[category.color.to_sym]
+    else
+      "--bs-secondary-bg"
+    end
+  end
 end
