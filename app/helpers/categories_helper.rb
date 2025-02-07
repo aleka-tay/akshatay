0591a486497ea986f82f8e7cae402f86da030f10
aleka-tay <almarwa.amirtay@gmail.com>
2025-02-07 16:24:19 +0100

    connect both scaffolds

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,11 +1,2 @@
 module CategoriesHelper
-  def colors_by_type(category)
-    colors={ pink: "--bs-danger-bg-subtle", green: "--bs-success-bg-subtle" }
-
-    if category&color.present?
-      colors[category.color.to_sym]
-    else
-      "--bs-secondary-bg"
-    end
-  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :expenses
+  validates :name, presence: true
+  validates :color, presence: true, inclusion: { in: [ "#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#F4C542", "#9B59B6" ], message: "must be one of the predefined colors" }
 end
