fce15e8c17e31745caf43a65388d8bda62a8f269
aleka-tay <almarwa.amirtay@gmail.com>
2025-02-12 16:13:57 +0100

    add graphs and chartkick

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,14 @@
 class Expense < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  belongs_to :category,  optional: true
+
+  before_validation :set_default_category, on: :create
+
+  private
+
+  def set_default_category
+    self.category ||= Category.find_by(title: "Miscellaneous") do |category|
+      category.color ||= "#CCCCCC"
+    end
+  end
 end
